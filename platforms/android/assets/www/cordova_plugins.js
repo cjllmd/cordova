5f1a41ba002ad733e9bbaf09864758c7152f9b1c
tianpy <13400717@qq.com>
2018-09-10 11:59:38 +0800

    'location'

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -39,6 +39,20 @@ module.exports = [
     "clobbers": [
       "CameraPopoverHandle"
     ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
   }
 ];
 module.exports.metadata = 
@@ -47,7 +61,8 @@ module.exports.metadata =
   "cordova-plugin-whitelist": "1.3.3",
   "x5webview-cordova-plugin": "1.1.0",
   "cordova-plugin-statusbar": "2.4.2",
-  "cordova-plugin-camera": "4.0.3"
+  "cordova-plugin-camera": "4.0.3",
+  "cordova-plugin-geolocation": "4.0.1"
 };
 // BOTTOM OF METADATA
 });
